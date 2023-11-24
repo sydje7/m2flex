0dacc2d97e6db604d3779ac64522a54eba46569a
sydje7 <144898932+sydje7@users.noreply.github.com>
2023-11-24 12:06:38 +0100

    sydney

---
+++ app.js
@@ -0,0 +1,10 @@
+let canvas = document.getElementById("canvas")
+let g = canvas.getContext("2d")
+const gamestate_start=0;
+const gamestate_ingame=1;
+const gamestate_gameover=2;
+
+
+const ingamestate_start=0;
+const ingamestate_roll=1;
+const ingamestate_end=0;
