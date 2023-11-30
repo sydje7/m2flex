c0eeedbe78c43844bf3c3aaf91953cb42f0584f2
sydje7 <144898932+sydje7@users.noreply.github.com>
2023-11-30 12:43:34 +0100

    sydney

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-let uiWindow = createRect(600,200,300,300);
 
+let uiWindow = createRect(600,200,300,300);
+ 
 const gamestate_start=0;
 const gamestate_ingame=1;
 const gamestate_gameover=2;
-
+ 
 const ingamestate_start=0;
-const ingamestate_roll=1;
+const ingamestate_roll=1
 const ingamestate_end=0;
-
+ 
 let boardPositionSize= 50;
-let pawnPositions= [];
+let pawnPosition= [];
 let boardPositions=[];
 let playerAmountButtons = [];
-
-let canvas = document.getElementById("canvas")
-let g = canvas.getContext("2d")
-
+ 
+let canvas = document.getElementById("canvas");
+let g = canvas.getContext("2d");
+ 
 function createRect(x,y,w,h)
 {
     let rectangle = {
@@ -27,62 +28,81 @@ function createRect(x,y,w,h)
         h:h
     };
     return rectangle;
-
-
-
 }
-function clearCanvas()
-{
-    g.fillStyle = "lightslategray"
+ function clearCanvas()
+ {
+    g.fillStyle = "lightgray";
     g.fillRect(0,0, canvas.width, canvas.height);
-}
-function draw()
+ 
+ }
+ 
+ function draw()
 {
     clearCanvas();
     for(let i =0 ; i<boardPositions.length;i++)
     {
         let pos = boardPositions[i];
-
+ 
         g.fillStyle  = "#004400";
-        //we gebruiken hier de x en y van het rectangle object
-        // vul bij de ??? ook de h & w in, net als bij de x en y gedaan is!
         g.fillRect(pos.x,pos.y,pos.w,pos.h);
         g.fillStyle  = "#FFFFFF";
         g.fillText((i+1)+"",pos.x,pos.y+20);
     }
 }
-function createBoardPositions()
-{
-    let x= 0;
-    let y = canvas.height-boardPositionSize;
-    let path = [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1] ;
-
-    for(let i =0 ; i<path.length;i++)
-    {
-
-        if(path[i] == 1)//gaan naar rechts
-        {
-            //bedenk hier wat je met de x moet doen
-           x += boardPositionSize;
-        }
-        else if(path[i] == 3)//gaan naar links
-        {
-            // bedenk hier wat je met de x moet doen
+function createBoardPositions() {
+    let x = 0;
+    let y = canvas.height -boardPositionSize;
+    let path = [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+ 
+    for (let i = 0; i < path.length; i++) {
+        if (path[i] == 1) {
+           
+            x += boardPositionSize;
+        } else if (path[i] == 3) {
+         
             x -= boardPositionSize;
-        }
-        else if(path[i] == 0)//gaan hier naar boven
-        {
-            //bedenk hier wat je met de y moet doen
+        } else if (path[i] == 0) {
+           
             y -= boardPositionSize;
         }
-        boardPositions.push(createRect(x,y,boardPositionSize,boardPositionSize));
+        boardPositions.push(createRect(x, y, boardPositionSize, boardPositionSize));
     }
-} 
-
-
-function initGame();
-let button = createRect(uiwindow.x +5 +,uiWindow.y+50,50,50);
-{
-    
-    createBoardPositions
 }
+ function initGame()
+ {
+    
+      
+    createBoardPositions
+ }
+
+ function draw() {
+    clearCanvas();
+    for (let i = 0; i < boardPositions.length; i++) {
+        let pos = boardPositions[i];
+
+        g.fillStyle = "#004400";
+        // Use the width and height properties of the rectangle object
+        g.fillRect(pos.x, pos.y, pos.width, pos.height);
+        
+        g.fillStyle = "#FFFFFF";
+        g.fillText((i + 1) + "", pos.x, pos.y + 20);
+    }
+}
+function createBoardPositions() {
+    let x = 0;
+    let y = canvas.height - boardPositionSize;
+    let path = [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+
+    for (let i = 0; i < path.length; i++) {
+        if (path[i] == 1) {
+            x += boardPositionSize; // Move right by the size of a board position
+        } else if (path[i] == 3) {
+            x -= boardPositionSize; // Move left by the size of a board position
+        } else if (path[i] == 0) {
+            y -= boardPositionSize; // Move up by the size of a board position
+        }
+        boardPositions.push(createRect(x, y, boardPositionSize, boardPositionSize));
+    }
+}
+createBoardPositions();
+draw();
